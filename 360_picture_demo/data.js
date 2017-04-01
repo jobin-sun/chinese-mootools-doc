30005b885f0a582da269423d94c6343bac8555e0
Jobin <sunbinj@gmail.com>
2017-04-02 02:01:07 +0800

    test

---
+++ 360_picture_demo/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.5846008373206582,
+          "pitch": -0.08351005750045815,
+          "title": "热点title",
+          "text": "热点内容<div>测试测试测试测试测试测试测试</div><div>测试测试测试<br></div><div>测试测试<br></div><div>测试<br></div><div>测试<br></div><div>测试测试<br></div><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
